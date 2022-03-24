4eff67992ef304e4709c7d0334a8188084e8af3b
Veks678 <Imeranor.Python@mail.ru>
2022-03-24 18:22:20 +0300

    refinement: implement uniformity

--- house_bookkeeper/apps/config.py
+++ house_bookkeeper/apps/config.py
@@ -1,4 +1,9 @@
-head_param = {'title': 'Анатолий Михеенко', 'encoding': 'UTF-8'}
+head_param = {
+    'title': 'Анатолий Михеенко',
+    'encoding': 'UTF-8',
+    'content': 'Сайт-визитка, Анатолий михеенко, резюме, блог, проекты, новости'
+
+}
 panel_param = [
     {'title': 'Новости', 'route_name': 'news'},
     {'title': 'Резюме', 'route_name': 'resume'},
